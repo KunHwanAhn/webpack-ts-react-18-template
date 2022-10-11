9c1e4f8117c568852e5710c6a12fd40fea1a15c3
Conan Kunhwan Ahn <kunhwon@gmail.com>
2022-10-11 13:18:48 +0900

    ES Lint와 tsconfig 수정 (#6)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,27 +3,49 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['react'],
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'airbnb',
     'airbnb/hooks',
-    'airbnb-typescript',
   ],
   settings: {
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx']
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
   },
   rules: {
-    'max-len': ["error", { "code": 150 }],
+    'max-len': ['error', { code: 150 }],
     'import/extensions': ['error', {
       ts: 'never',
       tsx: 'never',
+      json: 'always',
+      scss: 'always',
     }],
   },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint', 'react'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'airbnb-typescript',
+      ],
+      rules: {
+        'max-len': ['error', { code: 150 }],
+        'import/extensions': ['error', {
+          ts: 'never',
+          tsx: 'never',
+          json: 'always',
+          scss: 'always',
+        }],
+      },
+    },
+  ],
 };
